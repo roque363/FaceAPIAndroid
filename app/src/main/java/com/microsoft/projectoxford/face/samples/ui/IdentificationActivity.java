b5b9b37a39368d05f97beee34ac626d27ccc38a0
Roque Alarcon <roquealarcon363@gmail.com>
2018-11-29 22:14:08 -0500

    Main Changes

--- app/src/main/java/com/microsoft/projectoxford/face/samples/ui/IdentificationActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/face/samples/ui/IdentificationActivity.java
@@ -1,35 +1,3 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license.
-//
-// Microsoft Cognitive Services (formerly Project Oxford): https://www.microsoft.com/cognitive-services
-//
-// Microsoft Cognitive Services (formerly Project Oxford) GitHub:
-// https://github.com/Microsoft/Cognitive-Face-Android
-//
-// Copyright (c) Microsoft Corporation
-// All rights reserved.
-//
-// MIT License:
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
 package com.microsoft.projectoxford.face.samples.ui;
 
 import android.app.ProgressDialog;
@@ -107,7 +75,7 @@ public class IdentificationActivity extends AppCompatActivity {
                     return null;
                 }
 
-                publishProgress("Identifying...");
+                publishProgress("Identificando...");
 
                 // Start identification.
                 return faceServiceClient.identityInLargePersonGroup(
@@ -202,7 +170,7 @@ public class IdentificationActivity extends AppCompatActivity {
                     mPersonGroupId, IdentificationActivity.this);
             refreshIdentifyButtonEnabledStatus();
             textView.setTextColor(Color.BLACK);
-            textView.setText(String.format("Person group to use: %s", personGroupName));
+            textView.setText(String.format("Grupo escogido: %s", personGroupName));
         }
     }
 
@@ -296,10 +264,10 @@ public class IdentificationActivity extends AppCompatActivity {
 
                 if (result.length == 0) {
                     detected = false;
-                    setInfo("No faces detected!");
+                    setInfo("No se han detectado rostros!");
                 } else {
                     detected = true;
-                    setInfo("Click on the \"Identify\" button to identify the faces in image.");
+                    setInfo("Haga click en el botón \"Identificar\" para identificar las caras en la imagen.");
                 }
             } else {
                 detected = false;
@@ -389,7 +357,7 @@ public class IdentificationActivity extends AppCompatActivity {
                     faceIds.toArray(new UUID[faceIds.size()]));
         } else {
             // Not detected or person group exists.
-            setInfo("Please select an image and create a person group first.");
+            setInfo("Por favor, seleccione una imagen y cree un grupo de personas primero.");
         }
     }
 
@@ -523,8 +491,8 @@ public class IdentificationActivity extends AppCompatActivity {
                             mIdentifyResults.get(position).candidates.get(0).personId.toString();
                     String personName = StorageHelper.getPersonName(
                             personId, mPersonGroupId, IdentificationActivity.this);
-                    String identity = "Person: " + personName + "\n"
-                            + "Confidence: " + formatter.format(
+                    String identity = "Persona: " + personName + "\n"
+                            + "Certeza: " + formatter.format(
                             mIdentifyResults.get(position).candidates.get(0).confidence);
                     ((TextView) convertView.findViewById(R.id.text_detected_face)).setText(
                             identity);
